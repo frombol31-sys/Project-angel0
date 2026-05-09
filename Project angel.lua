10daa7707435153628ba2c25584b631889ec439b
frombol31-sys <frombol31@gmail.com>
2026-05-09 12:59:45 +0300

    Update Project angel.lua

--- Project angel.lua
+++ Project angel.lua
@@ -22,31 +22,30 @@ LMG2L["Frame_2"]["Size"] = UDim2.new(0.6425, 0, 0.74719, 0);
 LMG2L["Frame_2"]["Position"] = UDim2.new(0.235, 0, 0.11798, 0);
 
 
-LMG2L["Clear_3"] = Instance.new("TextButton", LMG2L["Frame_2"]);
-LMG2L["Clear_3"]["BorderSizePixel"] = 0;
-LMG2L["Clear_3"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["Clear_3"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
-LMG2L["Clear_3"]["Size"] = UDim2.new(0.09728, 0, 0.25564, 0);
-LMG2L["Clear_3"]["Text"] = [[CLEAR]];
-LMG2L["Clear_3"]["Name"] = [[Clear]];
-LMG2L["Clear_3"]["Position"] = UDim2.new(0.75097, 0, 0.32331, 0);
-
-
-LMG2L["LocalScript_4"] = Instance.new("LocalScript", LMG2L["Clear_3"]);
+LMG2L["LocalScript_3"] = Instance.new("LocalScript", LMG2L["Frame_2"]);
 
 
 
-LMG2L["Execute_5"] = Instance.new("TextButton", LMG2L["Frame_2"]);
-LMG2L["Execute_5"]["BorderSizePixel"] = 0;
-LMG2L["Execute_5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["Execute_5"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
-LMG2L["Execute_5"]["Size"] = UDim2.new(0.09339, 0, 0.21805, 0);
-LMG2L["Execute_5"]["Text"] = [[EXE]];
-LMG2L["Execute_5"]["Name"] = [[Execute]];
-LMG2L["Execute_5"]["Position"] = UDim2.new(0.75097, 0, 0.07519, 0);
+LMG2L["Project_4"] = Instance.new("TextLabel", LMG2L["Frame_2"]);
+LMG2L["Project_4"]["BorderSizePixel"] = 0;
+LMG2L["Project_4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["Project_4"]["Size"] = UDim2.new(0.16732, 0, 0.03759, 0);
+LMG2L["Project_4"]["Text"] = [[Project angel (client side)]];
+LMG2L["Project_4"]["Name"] = [[Project]];
+LMG2L["Project_4"]["Position"] = UDim2.new(0.03891, 0, 0.01504, 0);
 
 
-LMG2L["LocalScript_6"] = Instance.new("LocalScript", LMG2L["Execute_5"]);
+LMG2L["Clear_5"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["Clear_5"]["BorderSizePixel"] = 0;
+LMG2L["Clear_5"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["Clear_5"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
+LMG2L["Clear_5"]["Size"] = UDim2.new(0.09728, 0, 0.25564, 0);
+LMG2L["Clear_5"]["Text"] = [[CLEAR]];
+LMG2L["Clear_5"]["Name"] = [[Clear]];
+LMG2L["Clear_5"]["Position"] = UDim2.new(0.75097, 0, 0.32331, 0);
+
+
+LMG2L["LocalScript_6"] = Instance.new("LocalScript", LMG2L["Clear_5"]);
 
 
 
@@ -64,95 +63,150 @@ LMG2L["LocalScript_8"] = Instance.new("LocalScript", LMG2L["RE_7"]);
 
 
 
-LMG2L["Project_9"] = Instance.new("TextLabel", LMG2L["Frame_2"]);
-LMG2L["Project_9"]["BorderSizePixel"] = 0;
-LMG2L["Project_9"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["Project_9"]["Size"] = UDim2.new(0.16732, 0, 0.03759, 0);
-LMG2L["Project_9"]["Text"] = [[Project angel (client side)]];
-LMG2L["Project_9"]["Name"] = [[Project]];
-LMG2L["Project_9"]["Position"] = UDim2.new(0.03891, 0, 0.01504, 0);
+LMG2L["C00l_9"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["C00l_9"]["TextWrapped"] = true;
+LMG2L["C00l_9"]["BorderSizePixel"] = 0;
+LMG2L["C00l_9"]["TextScaled"] = true;
+LMG2L["C00l_9"]["BackgroundColor3"] = Color3.fromRGB(190, 0, 0);
+LMG2L["C00l_9"]["Size"] = UDim2.new(0.05447, 0, 0.11278, 0);
+LMG2L["C00l_9"]["Text"] = [[C00lgui]];
+LMG2L["C00l_9"]["Name"] = [[C00l]];
+LMG2L["C00l_9"]["Position"] = UDim2.new(0.75097, 0, 0.78195, 0);
 
 
-LMG2L["LocalScript2_a"] = Instance.new("LocalScript", LMG2L["Frame_2"]);
-LMG2L["LocalScript2_a"]["Name"] = [[LocalScript2]];
-
-
-LMG2L["R6_b"] = Instance.new("TextButton", LMG2L["Frame_2"]);
-LMG2L["R6_b"]["BorderSizePixel"] = 0;
-LMG2L["R6_b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["R6_b"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
-LMG2L["R6_b"]["Size"] = UDim2.new(0.06615, 0, 0.11278, 0);
-LMG2L["R6_b"]["Text"] = [[R6]];
-LMG2L["R6_b"]["Name"] = [[R6]];
-LMG2L["R6_b"]["Position"] = UDim2.new(0.85603, 0, 0.44361, 0);
-
-
-LMG2L["LocalScript_c"] = Instance.new("LocalScript", LMG2L["R6_b"]);
+LMG2L["LocalScript_a"] = Instance.new("LocalScript", LMG2L["C00l_9"]);
 
 
 
-LMG2L["TextLabel_d"] = Instance.new("TextLabel", LMG2L["Frame_2"]);
-LMG2L["TextLabel_d"]["TextWrapped"] = true;
-LMG2L["TextLabel_d"]["BorderSizePixel"] = 0;
-LMG2L["TextLabel_d"]["TextScaled"] = true;
-LMG2L["TextLabel_d"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["TextLabel_d"]["BackgroundTransparency"] = 1;
-LMG2L["TextLabel_d"]["Size"] = UDim2.new(0.45914, 0, 0.21805, 0);
-LMG2L["TextLabel_d"]["Text"] = [[Hi it’s executor made by sleepy_zzz100]];
-LMG2L["TextLabel_d"]["Position"] = UDim2.new(0.01946, 0, 0.76692, 0);
+LMG2L["LocalScript2_b"] = Instance.new("LocalScript", LMG2L["Frame_2"]);
+LMG2L["LocalScript2_b"]["Name"] = [[LocalScript2]];
 
 
-LMG2L["LocalScript_e"] = Instance.new("LocalScript", LMG2L["Frame_2"]);
+LMG2L["RO_c"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["RO_c"]["TextWrapped"] = true;
+LMG2L["RO_c"]["BorderSizePixel"] = 0;
+LMG2L["RO_c"]["TextScaled"] = true;
+LMG2L["RO_c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["RO_c"]["BackgroundColor3"] = Color3.fromRGB(65, 0, 208);
+LMG2L["RO_c"]["Size"] = UDim2.new(0.05447, 0, 0.11278, 0);
+LMG2L["RO_c"]["Text"] = [[Roxploit 6.0]];
+LMG2L["RO_c"]["Name"] = [[RO]];
+LMG2L["RO_c"]["Position"] = UDim2.new(0.82879, 0, 0.78195, 0);
 
 
-
-LMG2L["TextBox_f"] = Instance.new("TextBox", LMG2L["Frame_2"]);
-LMG2L["TextBox_f"]["BorderSizePixel"] = 0;
-LMG2L["TextBox_f"]["TextWrapped"] = true;
-LMG2L["TextBox_f"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
-LMG2L["TextBox_f"]["TextScaled"] = true;
-LMG2L["TextBox_f"]["BackgroundColor3"] = Color3.fromRGB(139, 139, 139);
-LMG2L["TextBox_f"]["Size"] = UDim2.new(0.71984, 0, 0.68421, 0);
-LMG2L["TextBox_f"]["Position"] = UDim2.new(0.01556, 0, 0.07519, 0);
-LMG2L["TextBox_f"]["Text"] = [[script]];
-
-
-LMG2L["C00l_10"] = Instance.new("TextButton", LMG2L["Frame_2"]);
-LMG2L["C00l_10"]["TextWrapped"] = true;
-LMG2L["C00l_10"]["BorderSizePixel"] = 0;
-LMG2L["C00l_10"]["TextScaled"] = true;
-LMG2L["C00l_10"]["BackgroundColor3"] = Color3.fromRGB(190, 0, 0);
-LMG2L["C00l_10"]["Size"] = UDim2.new(0.05447, 0, 0.11278, 0);
-LMG2L["C00l_10"]["Text"] = [[C00lgui]];
-LMG2L["C00l_10"]["Name"] = [[C00l]];
-LMG2L["C00l_10"]["Position"] = UDim2.new(0.75097, 0, 0.78195, 0);
-
-
-LMG2L["LocalScript_11"] = Instance.new("LocalScript", LMG2L["C00l_10"]);
+LMG2L["LocalScript_d"] = Instance.new("LocalScript", LMG2L["RO_c"]);
 
 
 
-local function C_4()
-	local script = LMG2L["LocalScript_4"];
+LMG2L["TextLabel_e"] = Instance.new("TextLabel", LMG2L["Frame_2"]);
+LMG2L["TextLabel_e"]["TextWrapped"] = true;
+LMG2L["TextLabel_e"]["BorderSizePixel"] = 0;
+LMG2L["TextLabel_e"]["TextScaled"] = true;
+LMG2L["TextLabel_e"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextLabel_e"]["BackgroundTransparency"] = 1;
+LMG2L["TextLabel_e"]["Size"] = UDim2.new(0.45914, 0, 0.21805, 0);
+LMG2L["TextLabel_e"]["Text"] = [[Hi it’s executor made by sleepy_zzz100]];
+LMG2L["TextLabel_e"]["Position"] = UDim2.new(0.01946, 0, 0.76692, 0);
+
+
+LMG2L["R6_f"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["R6_f"]["BorderSizePixel"] = 0;
+LMG2L["R6_f"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["R6_f"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
+LMG2L["R6_f"]["Size"] = UDim2.new(0.06615, 0, 0.11278, 0);
+LMG2L["R6_f"]["Text"] = [[R6]];
+LMG2L["R6_f"]["Name"] = [[R6]];
+LMG2L["R6_f"]["Position"] = UDim2.new(0.85603, 0, 0.44361, 0);
+
+
+LMG2L["LocalScript_10"] = Instance.new("LocalScript", LMG2L["R6_f"]);
+
+
+
+LMG2L["TextBox_11"] = Instance.new("TextBox", LMG2L["Frame_2"]);
+LMG2L["TextBox_11"]["BorderSizePixel"] = 0;
+LMG2L["TextBox_11"]["TextWrapped"] = true;
+LMG2L["TextBox_11"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["TextBox_11"]["TextScaled"] = true;
+LMG2L["TextBox_11"]["BackgroundColor3"] = Color3.fromRGB(139, 139, 139);
+LMG2L["TextBox_11"]["Size"] = UDim2.new(0.71984, 0, 0.68421, 0);
+LMG2L["TextBox_11"]["Position"] = UDim2.new(0.01556, 0, 0.07519, 0);
+LMG2L["TextBox_11"]["Text"] = [[script]];
+
+
+LMG2L["Execute_12"] = Instance.new("TextButton", LMG2L["Frame_2"]);
+LMG2L["Execute_12"]["BorderSizePixel"] = 0;
+LMG2L["Execute_12"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+LMG2L["Execute_12"]["BackgroundColor3"] = Color3.fromRGB(204, 204, 204);
+LMG2L["Execute_12"]["Size"] = UDim2.new(0.09339, 0, 0.21805, 0);
+LMG2L["Execute_12"]["Text"] = [[EXE]];
+LMG2L["Execute_12"]["Name"] = [[Execute]];
+LMG2L["Execute_12"]["Position"] = UDim2.new(0.75097, 0, 0.07519, 0);
+
+
+LMG2L["LocalScript_13"] = Instance.new("LocalScript", LMG2L["Execute_12"]);
+
+
+
+local function C_3()
+	local script = LMG2L["LocalScript_3"];
+	local UserInputService = game:GetService("UserInputService")	
+	local runService = game:GetService("RunService")	
+	
+	local gui = script.Parent	
+	
+	local dragging	
+	local dragInput	
+	local dragStart	
+	local startPos	
+	
+	local function update(input)	
+		local delta = input.Position - dragStart	
+		-- Используем TweenService для еще большей плавности или просто меняем Position	
+		gui.Position = UDim2.new(	
+			startPos.X.Scale, 	
+			startPos.X.Offset + delta.X, 	
+			startPos.Y.Scale, 	
+			startPos.Y.Offset + delta.Y	
+		)	
+	end	
+	
+	gui.InputBegan:Connect(function(input)	
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then	
+			dragging = true	
+			dragStart = input.Position	
+			startPos = gui.Position	
+	
+			-- Отключаем выделение текста или другие действия, пока тянем	
+			input.Changed:Connect(function()	
+				if input.UserInputState == Enum.UserInputState.End then	
+					dragging = false	
+				end	
+			end)	
+		end	
+	end)	
+	
+	gui.InputChanged:Connect(function(input)	
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then	
+			dragInput = input	
+		end	
+	end)	
+	
+	UserInputService.InputChanged:Connect(function(input)	
+		if input == dragInput and dragging then	
+			update(input)	
+		end	
+	end)	
+	
+end;
+task.spawn(C_3);
+local function C_6()
+	local script = LMG2L["LocalScript_6"];
 	
 	local button = script.Parent	
 	local textBox = button.Parent:WaitForChild("TextBox")	
 	button.MouseButton1Click:Connect(function()	
 	textBox.Text = ""	
-	end)	
-end;
-task.spawn(C_4);
-local function C_6()
-	local script = LMG2L["LocalScript_6"];
-	local button = script.Parent	
-	local textBox = button.Parent:WaitForChild("TextBox")	
-	button.MouseButton1Click:Connect(function()	
-	local code = textBox.Text	
-	if code ~= "" then	
-	pcall(function()	
-	loadstring(code)()	
-	end)	
-	end	
 	end)	
 end;
 task.spawn(C_6);
@@ -175,14 +229,35 @@ local function C_8()
 end;
 task.spawn(C_8);
 local function C_a()
-	local script = LMG2L["LocalScript2_a"];
+	local script = LMG2L["LocalScript_a"];
+	local button = script.Parent	
+	local textBox = button.Parent:WaitForChild("TextBox")	
+	
+	button.MouseButton1Click:Connect(function()	
+	    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-c00lgui-212097"))()	
+	end)	
+	
+end;
+task.spawn(C_a);
+local function C_b()
+	local script = LMG2L["LocalScript2_b"];
 	local sg = script:FindFirstAncestorOfClass("ScreenGui")	
 	if sg then sg.ResetOnSpawn = false end	
 	
 end;
-task.spawn(C_a);
-local function C_c()
-	local script = LMG2L["LocalScript_c"];
+task.spawn(C_b);
+local function C_d()
+	local script = LMG2L["LocalScript_d"];
+	local button = script.Parent	
+	local textBox = button.Parent:WaitForChild("TextBox")	
+	
+	button.MouseButton1Click:Connect(function()	
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/frombol31-sys/Project-angel0/refs/heads/main/Ro%20xploit.lua"))()	
+	
+end;
+task.spawn(C_d);
+local function C_10()
+	local script = LMG2L["LocalScript_10"];
 	local button = script.Parent	
 	local CharacterSystem = {}	
 	
@@ -236,69 +311,20 @@ local function C_c()
 	end)	
 	
 end;
-task.spawn(C_c);
-local function C_e()
-	local script = LMG2L["LocalScript_e"];
-	local UserInputService = game:GetService("UserInputService")	
-	local runService = game:GetService("RunService")	
-	
-	local gui = script.Parent	
-	
-	local dragging	
-	local dragInput	
-	local dragStart	
-	local startPos	
-	
-	local function update(input)	
-		local delta = input.Position - dragStart	
-		-- Используем TweenService для еще большей плавности или просто меняем Position	
-		gui.Position = UDim2.new(	
-			startPos.X.Scale, 	
-			startPos.X.Offset + delta.X, 	
-			startPos.Y.Scale, 	
-			startPos.Y.Offset + delta.Y	
-		)	
-	end	
-	
-	gui.InputBegan:Connect(function(input)	
-		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then	
-			dragging = true	
-			dragStart = input.Position	
-			startPos = gui.Position	
-	
-			-- Отключаем выделение текста или другие действия, пока тянем	
-			input.Changed:Connect(function()	
-				if input.UserInputState == Enum.UserInputState.End then	
-					dragging = false	
-				end	
-			end)	
-		end	
-	end)	
-	
-	gui.InputChanged:Connect(function(input)	
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then	
-			dragInput = input	
-		end	
-	end)	
-	
-	UserInputService.InputChanged:Connect(function(input)	
-		if input == dragInput and dragging then	
-			update(input)	
-		end	
-	end)	
-	
-end;
-task.spawn(C_e);
-local function C_11()
-	local script = LMG2L["LocalScript_11"];
+task.spawn(C_10);
+local function C_13()
+	local script = LMG2L["LocalScript_13"];
 	local button = script.Parent	
 	local textBox = button.Parent:WaitForChild("TextBox")	
-	
 	button.MouseButton1Click:Connect(function()	
-	    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-c00lgui-212097"))()	
+	local code = textBox.Text	
+	if code ~= "" then	
+	pcall(function()	
+	loadstring(code)()	
 	end)	
-	
+	end	
+	end)	
 end;
-task.spawn(C_11);
+task.spawn(C_13);
 
 return LMG2L["ScreenGui_1"], require;
